24cc9cb615de110f0931eb05ed9f58d46ba5447e
deny <deniansyah@erxhealth.co.uk>
2023-08-23 14:25:12 +0700

    .

--- lib/module/case/5_disabled_button/view/disabled_button_view.dart
+++ lib/module/case/5_disabled_button/view/disabled_button_view.dart
@@ -15,9 +15,94 @@ class DisabledButtonView extends StatefulWidget {
       ),
       body: SingleChildScrollView(
         child: Container(
-          padding: const EdgeInsets.all(10.0),
-          child: const Column(
-            children: [],
+          width: MediaQuery.of(context).size.width,
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            children: [
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.green,
+                ),
+                onPressed: () {},
+                child: const Text(
+                  "Save",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 20.0,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  ElevatedButton(
+                    onPressed: () {},
+                    child: const Text("Enable Button"),
+                  ),
+                  const SizedBox(
+                    width: 12.0,
+                  ),
+                  ElevatedButton(
+                    onPressed: () {},
+                    child: const Text("Disable Button"),
+                  ),
+                ],
+              ),
+              const SizedBox(
+                height: 20.0,
+              ),
+              Card(
+                child: Container(
+                  width: MediaQuery.of(context).size.width,
+                  padding: const EdgeInsets.all(8.0),
+                  child: const Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "Instruksi",
+                        style: TextStyle(
+                          fontSize: 14.0,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.red,
+                        ),
+                      ),
+                      Divider(),
+                      Text(
+                        """
+State
+bool enabled = false;
+
+Tambahkan method:
+enableButton(){}
+disableButton(){}
+
+Method enableButton,
+akan mengubah nilai enabled menjadi true
+
+Method disableButton,
+akan mengubah nilai enabled menjadi false
+
+Panggil method enableButton ketika tombol EnableButton di click
+Panggil method disableButton ketika tombol DisableButton di click
+
+UI Effect:
+Ubah warna button menjadi Colors.grey[400] dengan textColor Colors.grey[900]
+jika enabled = false
+Ubah warna button menjadi Colors.green dengan textColor Colors.white
+jika enabled = true
+      """,
+                        style: TextStyle(
+                          fontSize: 14.0,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
           ),
         ),
       ),
